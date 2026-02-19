d482b87baf590857a9773122154f9a3a2fd3a87a
Matthew Sciberras <matthew.sciberras800@gmail.com>
2026-02-19 21:30:20 +0100

    Commit 19/02/2026

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [RouterLink]
 })
 export class LoginComponent { }
