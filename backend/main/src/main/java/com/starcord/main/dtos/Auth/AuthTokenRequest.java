ab270a3f9b9ade22f5f3bd43c05b95d7c842905b
Matthew Sciberras <matthew.sciberras800@gmail.com>
2026-03-02 16:25:25 +0100

    Finalized backend for how refresh and access tokens are sent/received

--- backend/main/src/main/java/com/starcord/main/dtos/Auth/AuthTokenRequest.java
+++
@@ -1,8 +0,0 @@
-package com.starcord.main.dtos.Auth;
-
-public class AuthTokenRequest {
-    private String refreshToken;
-
-    public String getRefreshToken() { return refreshToken; }
-    public void setRefreshToken(String refreshToken) { this.refreshToken = refreshToken; }
-}
