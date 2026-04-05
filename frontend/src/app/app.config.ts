24392f4709bb492c2ce8b5efca9521921933dead
Matthew Sciberras <matthew.sciberras800@gmail.com>
2026-04-05 13:21:32 +0200

    Updates to config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,18 +1,25 @@
 import {ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/auth/auth.interceptor';
-import {LogOut, LucideAngularModule, Plus, SendHorizontal, Settings, Users} from 'lucide-angular';
+import { authInterceptor } from '@core/auth/auth.interceptor';
+import {Link, LogOut, LucideAngularModule, Plus, SendHorizontal, Settings, Users} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      })
+    ),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
-    importProvidersFrom(LucideAngularModule.pick({ LogOut, Users, Settings, Plus, SendHorizontal }))
+    importProvidersFrom(LucideAngularModule.pick({ LogOut, Users, Settings, Plus, SendHorizontal, Link }))
   ]
 };
